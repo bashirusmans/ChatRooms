c3cad32d8722cf6cbbddda05decccd4eb5e7e8c0
bashirusmans <bashirusman3981@gmail.com>
2023-12-03 15:59:52 +0500

    Topic, users and message

--- darwaazay/doors/admin.py
+++ darwaazay/doors/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+admin.site.register(models.Topic)
 admin.site.register(models.Room)
+admin.site.register(models.Message)
