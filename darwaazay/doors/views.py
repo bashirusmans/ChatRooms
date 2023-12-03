a7d8d67b9f7a1b14d9e00fc645c4b28a0ae002f7
bashirusmans <bashirusman3981@gmail.com>
2023-12-03 14:40:34 +0500

    models and database setup

--- darwaazay/doors/views.py
+++ darwaazay/doors/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from . import models
 # Create your views here.
 
 def home(request):
-    return render(request, 'doors/home.html')
+    rooms = models.Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'doors/home.html', context)
 
-def room(request):
-    return render(request, 'doors/room.html')
+def room(request, pk):
+    room = models.Room.objects.get(id=int(pk))
+    context = {'room':room}
+    return render(request, 'doors/room.html', context)
