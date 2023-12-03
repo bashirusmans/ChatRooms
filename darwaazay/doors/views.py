b30d6a2bc8c3264d8e2d07cf624a5abd6053696b
bashirusmans <bashirusman3981@gmail.com>
2023-12-03 13:14:26 +0500

    templates init

--- darwaazay/doors/views.py
+++ darwaazay/doors/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'doors/home.html')
+
+def room(request):
+    return render(request, 'doors/room.html')
