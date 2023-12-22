4c52e5dcaecd68068ad9a7f24d0dd97ad69675a5
am-i-hira <hiratoheed2002@gmail.com>
2023-12-22 15:09:07 +0500

    testing

--- darwaazay/doors/tests.py
+++ darwaazay/doors/tests.py
@@ -1,5 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.test import SimpleTestCase
+from django.urls import resolve,reverse
+from doors.views import *
+from doors.models import *
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+from doors.forms import *
+from django.core.exceptions import ValidationError  # Import ValidationError
+from datetime import timedelta  # Import timedelta
 
 
 # Create your tests here.
@@ -356,3 +365,285 @@ class TestViews(TestCase):
             "which means view has been rendered.",
         )
         self.assertTemplateUsed(response, "doors/activity.html")
+
+
+
+
+
+
+# ------------------------------------testing urls --------------------------------------
+
+class TestUrls(SimpleTestCase):
+    
+    
+    # Test that the login URL resolves to the loginPage view
+    def test_login_url_resolves(self):
+        url = reverse('login')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, loginPage)
+
+    # Test that the logout URL resolves to the logoutUser view
+    def test_logout_url_resolves(self):
+        url = reverse('logout')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, logoutUser)
+
+    # Test that the register URL resolves to the registerUser view
+    def test_register_url_resolves(self):
+        url = reverse('register')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, registerUser)
+
+    # Test that the update-user URL resolves to the updateUser view
+    def test_update_user_url_resolves(self):
+        url = reverse('update-user')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, updateUser)
+
+    # Test that the home URL resolves to the home view
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, home)
+
+    # Test that the room URL resolves to the room view
+    def test_room_url_resolves(self):
+        url = reverse('room', args=['some_pk'])
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, room)
+
+    # Test that the user-profile URL resolves to the userProfile view
+    def test_user_profile_url_resolves(self):
+        url = reverse('user-profile', args=['some_pk'])
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, userProfile)
+
+    # Test that the create-room URL resolves to the createRoom view
+    def test_create_room_url_resolves(self):
+        url = reverse('create-room')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, createRoom)
+
+    # Test that the update-room URL resolves to the updateRoom view
+    def test_update_room_url_resolves(self):
+        url = reverse('update-room', args=['some_pk'])
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, updateRoom)
+
+    # Test that the delete-room URL resolves to the deleteRoom view
+    def test_delete_room_url_resolves(self):
+        url = reverse('delete-room', args=['some_pk'])
+        resolver = resolve(url)   
+        self.assertEquals(resolver.func, deleteRoom)
+
+    # Test that the delete-message URL resolves to the deleteMessage view
+    def test_delete_message_url_resolves(self):
+        url = reverse('delete-message', args=['some_pk'])
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, deleteMessage)
+
+    # Test that the topics URL resolves to the topicsPage view
+    def test_topics_url_resolves(self):
+        url = reverse('topics')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, topicsPage)
+
+    # Test that the activity URL resolves to the activityPage view
+    def test_activity_url_resolves(self):
+        url = reverse('activity')
+        resolver = resolve(url)
+        self.assertEquals(resolver.func, activityPage)
+
+
+
+
+# ---------------------------------------------- testing modals ---------------------------------
+class TestModels(TestCase):
+    class MessageModelTests(TestCase):
+
+        def setUp(self):
+            self.user = User.objects.create_user(username='testuser', password='testpassword')
+            self.room = Room.objects.create(name='Test Room', host=self.user)
+
+        def test_create_message_with_valid_data(self):
+            message = Message.objects.create(
+                user=self.user,
+                room=self.room,
+                body='This is a test message'
+            )
+            self.assertEqual(message.user, self.user)
+            self.assertEqual(message.room, self.room)
+            self.assertEqual(message.body, 'This is a test message')
+
+                
+
+        def test_str_method(self):
+            message = Message.objects.create(user=self.user, room=self.room, body='Fever signals immune response; often caused by infections or underlying conditions.')
+            self.assertEqual(str(message), 'Fever signals immune response; often caused by inf')  # Truncated to 50 characters
+            
+         
+        
+        
+# --------------------testing formss---------------
+class TestForms(SimpleTestCase):
+    class MyUserCreationFormTest(TestCase):
+
+        def test_valid_form(self):
+            # Create a valid form with dummy data
+            form_data = {
+                'name': 'Test User',
+                'username': 'test_user',
+                'email': 'test@example.com',
+                'password1': 'Password.123',
+                'password2': 'Password.123',
+            }
+            form = MyUserCreationForm(data=form_data)
+
+            # Print form errors for debugging
+            if not form.is_valid():
+                print(form.errors)
+
+            # Assert that the form is valid
+            self.assertTrue(form.is_valid())
+
+
+        def test_required_fields(self):
+            # Test that all required fields are present
+            form = MyUserCreationForm(data={})
+            self.assertFalse(form.is_valid())
+            self.assertIn('name', form.errors)
+            self.assertIn('username', form.errors)
+            self.assertIn('email', form.errors)
+            self.assertIn('password1', form.errors)
+            self.assertIn('password2', form.errors)
+
+        def test_password_mismatch(self):
+            # Test that the form detects password mismatch
+            form_data = {
+                'name': 'Test User',
+                'username': 'test_user',
+                'email': 'test@example.com',
+                'password1': 'password123',
+                'password2': 'different_password',
+            }
+            form = MyUserCreationForm(data=form_data)
+            self.assertFalse(form.is_valid())
+            self.assertIn('password2', form.errors)
+
+        def test_email_unique_constraint(self):
+            # Test that the form enforces the unique constraint on email
+            existing_user = get_user_model().objects.create_user(
+                name='Existing User',
+                username='existing_user',
+                email='existing@example.com',
+                password='existing_password'
+            )
+
+            form_data = {
+                'name': 'Test User',
+                'username': 'test_user',
+                'email': 'existing@example.com',  # This email is already taken
+                'password1': 'password123',
+                'password2': 'password123',
+            }
+            form = MyUserCreationForm(data=form_data)
+            self.assertFalse(form.is_valid())
+            self.assertIn('email', form.errors) 
+            
+    # >>>>>>>><<<<<<<<<<<<<<<<<<<<<<
+    class RoomFormTest(TestCase):
+        
+        def setUp(self):
+            self.user = User.objects.create_user(username='testuser', password='testpassword')
+            self.topic = Topic.objects.create(name='Test Topic')
+
+        def test_create_room_with_valid_data(self):
+            room = Room.objects.create(
+                name='Test Room',
+                host=self.user,
+                topic=self.topic,
+                description='This is a test room description'
+            )
+            self.assertEqual(room.name, 'Test Room')
+            self.assertEqual(room.host, self.user)
+            self.assertEqual(room.topic, self.topic)
+            self.assertEqual(room.description, 'This is a test room description')
+
+
+        def test_remove_host_or_topic(self):
+            room = Room.objects.create(name='Test Room', host=self.user, topic=self.topic)
+            self.user.delete()
+            self.topic.delete()
+            room.refresh_from_db()
+            self.assertIsNone(room.host)
+            self.assertIsNone(room.topic)
+
+        def test_ordering(self):
+            room1 = Room.objects.create(name='Room 1', host=self.user, topic=self.topic)
+            room2 = Room.objects.create(name='Room 2', host=self.user, topic=self.topic)
+            room2.updated = room2.updated - timedelta(days=1)  # Modify update time
+            rooms = Room.objects.all()
+            self.assertEqual(list(rooms), [room2, room1])  # Ordered by -updated, then -created
+
+        def test_str_method(self):
+            room = Room.objects.create(name='Test Room', host=self.user, topic=self.topic)
+            self.assertEqual(str(room), 'Test Room')
+
+        def test_form_clean_data(self):
+            # Test any custom cleaning logic in the form's clean() method
+            pass  # Implement specific tests for cleaning logic
+    class UserFormTest(TestCase):
+
+        def test_valid_form(self):
+            form_data = {
+                'avatar': 'test_avatar.png',
+                'name': 'Test User',
+                'username': 'test_username',
+                'email': 'test@example.com',
+                'bio': 'This is a test bio',
+            }
+            form = UserForm(data=form_data)
+            self.assertTrue(form.is_valid())
+
+        def test_blank_data(self):
+            form = UserForm(data={})
+            self.assertFalse(form.is_valid())
+            self.assertEqual(len(form.errors), 4)  # Assuming there are 4 required fields in the form
+
+        def test_invalid_email(self):
+            form_data = {
+                'avatar': 'test_avatar.png',
+                'name': 'Test User',
+                'username': 'test_username',
+                'email': 'invalid_email',  # Invalid email
+                'bio': 'This is a test bio',
+            }
+            form = UserForm(data=form_data)
+            self.assertFalse(form.is_valid())
+            self.assertIn('email', form.errors)
+
+        def test_form_save(self):
+            form_data = {
+                'avatar': 'test_avatar.png',
+                'name': 'Test User',
+                'username': 'test_username',
+                'email': 'test@example.com',
+                'bio': 'This is a test bio',
+            }
+            form = UserForm(data=form_data)
+            self.assertTrue(form.is_valid())
+
+            # Assuming you have overridden the save method in your User model
+            user_instance = form.save(commit=False)
+            self.assertEqual(user_instance.name, 'Test User')
+            self.assertEqual(user_instance.username, 'test_username')
+            self.assertEqual(user_instance.email, 'test@example.com')
+            self.assertEqual(user_instance.bio, 'This is a test bio')
+            # Add more assertions based on your model fields
+
+            # Save the instance to the database
+            user_instance.save()
+
+            # Retrieve the instance from the database and assert its values
+            saved_user = User.objects.get(pk=user_instance.pk)
+            self.assertEqual(saved_user.name, 'Test User')
